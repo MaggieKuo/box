a9033e7f06a08f6cbfb489a269610f4787604c45
MaggieKuo <perfumekuo@gmail.com>
2018-12-29 23:48:13 +0800

    when

--- src/main/kotlin/com/box/BoxTester.kt
+++ src/main/kotlin/com/box/BoxTester.kt
@@ -4,30 +4,44 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
-    val length = scanner.input("Please enter object's length: ", {scanner.nextFloat()})
-    val width = scanner.input("Please enter object's width: ", {scanner.nextFloat()})
+    val length = scanner.input("Please enter object's length: "){scanner.nextFloat()}
+    val width = scanner.input("Please enter object's width: "){scanner.nextFloat()}
     val height = scanner.input("Please enter object's width: ", {scanner.nextInt()})
 
-    when{
-        Box3().validate(length, width, height) -> println(Box3.BOX_NAME)
-        Box5().validate(length, width, height) -> println(Box5.BOX_NAME)
-    }
+    var box = Box.getInstance(length, width, height)
+    println(box?.getName())
 }
 
-interface Box{
+interface IBox{
     var length: Float
     var width: Float
     val height: Int
     fun validate(length: Float, width : Float, height : Int): Boolean
+    fun getName(): String
 }
 
-open class BaseBox(override var length: Float,
+class Box{
+    companion object {
+        fun getInstance(length: Float, width : Float, height : Int): BaseBox? {
+            var box: BaseBox? = null
+            when{
+                Box3().validate(length, width, height) -> box = Box3()
+                Box5().validate(length, width, height) -> box = Box5()
+            }
+            return box
+        }
+    }
+}
+
+open abstract class BaseBox(override var length: Float,
                    override var width: Float,
-                   override val height: Int) : Box {
+                   override val height: Int) : IBox {
+
     init {
         if (width > length)
             length = width.also { width = length }
     }
+
     override fun validate(length: Float, width: Float, height: Int): Boolean {
         val mLength = maxOf(length, width)
         var mWidth = minOf(length, width)
@@ -36,13 +50,13 @@ open class BaseBox(override var length: Float,
 }
 
 class Box3(): BaseBox(23f, 14f, 13){
-    companion object {
-        val BOX_NAME = "Box3"
+    override fun getName(): String {
+        return "Box3"
     }
 }
 class Box5(): BaseBox(39.5f, 27.5f, 23){
-    companion object {
-        val BOX_NAME = "Box5"
+    override fun getName(): String {
+        return "Box5"
     }
 }
 
